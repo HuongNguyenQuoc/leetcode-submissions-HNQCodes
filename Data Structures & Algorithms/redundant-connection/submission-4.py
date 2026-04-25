d9bcabe885db599d022f1b1d0cca9d50e88dc3e5
Huong Nguyen Quoc <quochuong05ln@gmail.com>
2026-04-25 11:11:37 +0700

    Add: redundant-connection - submission-4

---
+++ Data Structures & Algorithms/redundant-connection/submission-4.py
@@ -0,0 +1,60 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        '''
+        n = len(edges) #Number of edge
+        parent = list(range(n + 1))
+        size = [1] * (n + 1)
+
+        def find(x: int) -> int:
+            if parent[x] != x:
+                parent[x] = find(parent[x]) #path compression
+            return parent[x]
+        
+        def union(a: int, b: int) -> bool:
+            root_a, root_b = find(a), find(b)
+
+            if root_a == root_b:
+                return False #cycle detected
+            
+            #union by size
+            if size[root_a] < size[root_b]:
+                parent[root_a] = root_b
+                size[root_b] += size[root_a]
+            else:
+                parent[root_b] = root_a
+                size[root_b] += size[root_a]
+
+            return True    
+        
+        for u, v in edges:
+            if not union(u, v): #cycle detected
+                return [u, v]
+        '''
+        n = len(edges)
+        parent = list(range(n + 1))
+        size = [1] * (n + 1)
+
+        def find(n: int) -> int:
+            if parent[n] != n:
+                parent[n] = find(parent[n])
+            
+            return parent[n]
+            
+        def union(a: int, b: int) -> bool:
+            ra, rb = find(a), find(b)
+            if ra == rb:
+                return False
+            else:
+                if size[ra] > size[rb]:
+                    parent[rb] = ra
+                    size[ra] += size[rb]
+                else:
+                    parent[ra] = rb
+                    size[rb] += size[ra]
+            return True
+        
+        for u, v in edges:
+            if not union(u, v):
+                return [u, v]
+            
+        
