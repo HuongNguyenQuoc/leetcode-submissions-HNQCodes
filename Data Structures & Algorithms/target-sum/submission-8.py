6679104d3cdeca493ba3e4352155b1b9ec5738a2
Huong Nguyen Quoc <ct080228@actvn.edu.vn>
2026-07-24 16:05:32 +0700

    Add: target-sum - submission-8

---
+++ Data Structures & Algorithms/target-sum/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        if abs(target) > sum(nums):
+            return 0
+        dp = {}
+
+        def backtrack(index, current_sum):
+            if (index, current_sum) in dp:
+                return dp[(index, current_sum)]
+            if index == n:
+                return 1 if current_sum == target else 0
+
+            add = backtrack(index + 1, current_sum + nums[index])
+            subtract = backtrack(index + 1, current_sum - nums[index])
+            result = add + subtract
+            dp[(index, current_sum)] = result
+            
+            return dp[(index, current_sum)]
+
+        return backtrack(0, 0)
