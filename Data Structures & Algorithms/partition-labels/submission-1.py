fc0b4fecd6129b46a9dd5f33fa675f8e1b62708c
Quoc Huong Nguyen <ct080228@actvn.edu.vn>
2026-09-24 11:17:18 +0700

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last, res = {}, []
+        end, cnt = 0, 0
+        
+        # Build hashMap for each letter we need find the last index it's show up in original string
+        for i, ch in enumerate(s):
+            last[ch] = i
+        
+        # We need check where the portition match index so we add them into res
+        for i, ch in enumerate(s):
+            end = max(end, last[ch])
+            cnt += 1
+            if i == end:
+                res.append(cnt)
+                end, cnt = -1, 0
+        
+        return res
